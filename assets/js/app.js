752d0e1de805278cb4275ca9d30386314c7314f5
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-04 11:53:10 -0800

    creates textbox only when event col is empty

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,51 @@ const event17 = $("#17");
 
 let events = [];
 
-$()
+const createEvent = function() {
+    
 
-// edit events already on the calendar 
-$(".event").on("click", "p", function() {
-    console.log(eventTime);
-    let text = $(this).text().trim();
-    var textInput = $("<textarea>").val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
+    // grab inputted value of text area
+    
+
+    // append text area to event col
+    // $("#"+ rowId)
+};
+
+// createEvent();
+
+// event col click functionality
+$(".event").on("click", function() {
+    // check if textarea already present
+    let length = $(this).html().length
+    console.log(length)
+    if(length == 0) {
+        // create text area
+        let textAreaEl = $("<textarea>").val("enter your event");
+
+        // save input value
+        let text = textAreaEl.val().trim();
+        console.log(text);
+
+        // highlight text box
+        textAreaEl.trigger("focus");
+
+        // get id of event
+        let rowId = $(this).attr("id");
+
+        // append text area to selected event col 
+        $("#" + rowId).append(textAreaEl);
+    } else {
+        console.log("not empty")
+    }
+});
+
+// save button functionality
+$(".row").on("click", ".saveBtn", function() {
+    // grab event text
+    console.log(this)
+    let text =$(".event").attr("id");
+    console.log(text);
+
+    // create p element to save event
+    let eventP = $("<p>").val
 });
