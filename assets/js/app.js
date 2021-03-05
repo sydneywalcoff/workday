482a59ff80109f3178ed29552c976745d16d3cb2
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-05 10:47:35 -0800

    save button now takes textarea input and saves it to calendar as  element

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,12 @@ const createEvent = rowId => {
    // create text area
    let textAreaEl = $("<textarea>").val("enter your event");
 
-   // save input value
-   let text = textAreaEl.val().trim();
-   console.log(text);
-
    // highlight text box
    textAreaEl.trigger("focus");
 
    // append text area to selected event col 
    $("#" + rowId).append(textAreaEl);
 
-   $(".saveBtn").on("click", saveButton(rowId));
 };
 
 const editEvent = rowId => {
@@ -24,10 +19,15 @@ const editEvent = rowId => {
 };
 
 const saveButton = (rowId) => {
-    console.log(rowId);
     // grab event text from `.event col` textarea
+    let event = $("#" + rowId + " textarea");
+    let eventText = event.val();
 
     // turn into `p` element
+    let savedEventEl = $("<p>").text(eventText);
+    event.replaceWith(savedEventEl)
+
+    
 };
 
 // event col click functionality
